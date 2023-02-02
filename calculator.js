6a5474c4aec53c1e05e18f319947c2b5010b5119
Aye Chan <ayechan.sant@gmail.com>
2023-02-02 15:19:29 -0500

    refactor

--- calculator.js
+++ calculator.js
@@ -43,46 +43,63 @@ function operate(operation, a, b) {
     return operation(a, b);
 }
 
-//important variables
 let buttons = document.querySelectorAll('.btn');
-
+let a;
+let b;
 let prepareFirstNumber = [];
 let prepareSecondNumber = [];
 let standbyForOperation = [];
 
+//helper functions
+function clearCalculator() {
+    prepareFirstNumber, prepareSecondNumber, standbyForOperation = [];
+    a, b = null;
+    display.innerHTML = 'Wrath of God';
+}
+
+//while operation button is not clicked, keep adding nums to a list 
+
+function buildNumber() {
+    let prepareNumber = [];    
+    buttons.forEach((button) => {
+        button.addEventListener('click', function() {
+            display.innerHTML = `${button.innerHTML}`;
+            while (isNaN(button.innerHTML)) {
+                prepareNumber.push(button.innerHTML);
+            }
+            let number = ''; 
+            prepareNumber.forEach((num) => number += num);     //trying to have one function that just makes a number
+            console.log(prepareNumber);
+            console.log(Number(number));
+            return Number(number);
+        })
+    })
+}
+buildNumber();
 
 buttons.forEach((button) => {
     button.addEventListener('click', function() {   //everytime is button is clicked, do this:
         display.innerHTML = `${button.innerHTML}`;
-        let a;
-        let b;
 
         //need to find a way to switch between accepting numbers and operations. try array.lengths?
 
-        if (!isNaN(button.innerHTML)) {             
+        if (!isNaN(button.innerHTML) && standbyForOperation.length < 2) {             
             prepareFirstNumber.push(button.innerHTML);
             console.log(prepareFirstNumber);      
-        } else if (isNaN(button.innerHTML)) {     
-            let firstNumber = ''        
+        } else if (isNaN(button.innerHTML) && button.innerHTML !== 'equals') {     
+            let firstNumber = '';        
             prepareFirstNumber.forEach((num) => firstNumber += num);
             a = Number(firstNumber);    
             console.log(standbyForOperation);       
-        }
-
-        let alreadyHasAnOperator = false;
-        standbyForOperation.forEach((item) => {
-            if (typeof item === 'string') {
-                alreadyHasAnOperator = true;
-            }
-        })
-        
-        if (alreadyHasAnOperator) {
-            let secondNumber = ''        
-            prepareSecondNumber.forEach((num) => firstNumber += num);
+        } else if (!isNaN(button.innerHTML) && standbyForOperation.length < 2) {
+            let secondNumber = '';        
+            prepareSecondNumber.forEach((num) => secondNumber += num);
             b = Number(secondNumber);
             standbyForOperation.push(String(button.innerHTML), a, b);
             console.log(standbyForOperation);
+        } else if (button.innerHTML === 'equals') {
+            let operation = standbyForOperation[1];
+            operate(operation, a, b);
         }
     })
 })
-
